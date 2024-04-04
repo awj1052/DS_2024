1a4ea6e26cc45384bcdb510c279acba698102e75
brian <brian951862@naver.com>
2024-04-04 15:23:33 +0900

    Update README.md

--- src/main/CDList.java
+++ src/main/CDList.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.NoSuchElementException;
+
 public class CDList<E> {
 
     private DNode<E> head;
@@ -28,19 +30,45 @@ public class CDList<E> {
 
     public boolean isEmpty() {
         // TODO CDList가 비어있는지 여부를 반환하시오.
+        if (size == 0) return true;
         return false;
     }
 
     public void insert(int index, E newItem) {
-        // TODO head가 0번째 일 때 index번 째에 newItem 값을 삽입하도록 구현하시오.
         // TODO index가 올바르지 않을 때 IndexOutOfBoundsException 예외를 발생시키시오.
-        // 추가
+        if (index < 0 || size < index) throw new IndexOutOfBoundsException();
+        // TODO head가 0번째 일 때 index번 째에 newItem 값을 삽입하도록 구현하시오.
+        if (size == 0) {
+            head = new DNode<E>(newItem);
+            size++;
+        } else {
+            DNode<E> temp = head;
+            int i = 0;
+            while (index == i) {
+                temp = temp.getNext();
+                i++;
+            }
+            DNode<E> nextNode = temp.getNext();
+            DNode<E> nowNode = new DNode<E>(newItem);
+            nowNode.setNext(nextNode);
+            temp.setNext(nowNode);
+        }
     }
 
     public E delete(int index) {
-        // TODO head가 0번째 일 때 index번 째의 노드의 data를 반환하고 삭제하시오.
-        // TODO 리스트가 비어있을 때 NoSuchElementException 예외를 발생시키시오.
         // TODO index가 올바르지 않을 때 IndexOutOfBoundsException 예외를 발생시키시오.
+        if (index < 0 || size < index) throw new IndexOutOfBoundsException();
+        // TODO 리스트가 비어있을 때 NoSuchElementException 예외를 발생시키시오.
+        if (isEmpty()) throw new NoSuchElementException();
+        // TODO head가 0번째 일 때 index번 째의 노드의 data를 반환하고 삭제하시오.
+        if (size == 0) {
+            DNode<E> temp = head;
+            head = null;
+            return temp.getData();
+        } else {
+
+        }
+
         return null;
     }
 
