841720d4acfb0579e6ce309fa1288a8baca2b2fd
brian <brian951862@naver.com>
2024-04-12 12:00:01 +0900

    完

--- src/main/BinaryTreeTravel.java
+++ src/main/BinaryTreeTravel.java
@@ -31,6 +31,10 @@ public class BinaryTreeTravel<Key extends Comparable<Key>> {
 
     private void inOrder(Node<Key> node, List<Key> list) {
         // TODO preOrder를 참고하여 해당 메소드를 완성시키시오.
+        if (node == null) return;
+        inOrder(node.getLeft(), list);
+        list.add(node.getItem());
+        inOrder(node.getRight(), list);
     }
 
     public List<Key> postOrder() {
@@ -41,6 +45,10 @@ public class BinaryTreeTravel<Key extends Comparable<Key>> {
 
     private void postOrder(Node<Key> node, List<Key> list) {
         // TODO preOrder를 참고하여 해당 메소드를 완성시키시오.
+        if (node == null) return;
+        postOrder(node.getLeft(), list);
+        postOrder(node.getRight(), list);
+        list.add(node.getItem());
     }
 
     public List<Key> levelOrder() {
@@ -51,5 +59,18 @@ public class BinaryTreeTravel<Key extends Comparable<Key>> {
 
     private void levelOrder(Node<Key> node, List<Key> list) {
         // TODO java.util.Queue 와 자료구조 PDF를 참고하여 해당 메소드를 완성시키시오.
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            Node temp = queue.poll();
+            list.add((Key) temp.getItem());
+            if (temp.getLeft() != null) {
+                queue.add(temp.getLeft());
+            }
+            if (temp.getRight() != null) {
+                queue.add(temp.getRight());
+            }
+        }
     }
 }
