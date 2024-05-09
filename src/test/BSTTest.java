7ec779baa514d4e21ace3615eb06e074123ce59b
awj1052 <awj1052@naver.com>
2024-05-09 14:26:36 +0900

    Refactor: BSTTest import

--- src/test/BSTTest.java
+++ src/test/BSTTest.java
@@ -6,10 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class BSTTest {
 
